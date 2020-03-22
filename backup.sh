64a93e66c4934b8060a6599c2589a2d68bc4b253
root <root@vmd49703.contaboserver.net>
2020-03-22 10:57:54 +0100

    Rewrote backup script

--- backup.sh
+++ backup.sh
@@ -1,18 +1,37 @@
 #!/bin/bash
 
-DATE=$(date +"%d-%m-%Y")
+DATE=$(date +"%d-%m-%Y-%H:%M")
 FILENAME="minecraft-wrld-$DATE.tar.gz"
 BACKUPDIR=../mc-backups
 MCDIR=$(pwd)
 CRONFILE="/etc/cron.d/minecraft-backup"
+BACKUPSCRIPT="mc-backup.sh"
 
 echo -e "Creating Backup directory..."
 mkdir -p $BACKUPDIR
 cd $BACKUPDIR && BACKUPFULLPATH=$(pwd)
 echo -e "Installing cron..."
 cat > $CRONFILE << EOF
-0 4 \* \* \* tar -czf $BACKUPFULLPATH/$FILENAME $MCDIR/world*
+0 */6 * * * $BACKUPFULLPATH/$BACKUPSCRIPT
 EOF
 
+echo -e "Creating backup script"
+cat > $BACKUPFULLPATH/$BACKUPSCRIPT << EOF
+#!/bin/bash
+DATE=$DATE
+FILENAME=$FILENAME
+BACKUPDIR=$BACKUPFULLPATH
+MCDIR=$MCDIR
+LIMIT="90"
+
+# Backup
+tar -czf \$BACKUPDIR/\$FILENAME \$MCDIR/world*
+# Delete files older than \$LIMIT days
+find \$BACKUPDIR -name "*.tar.gz" -type f -mtime +\$LIMIT -exec rm -f {} \;
+EOF
+
+echo -e "Making executable"
+chmod +x $BACKUPFULLPATH/$BACKUPSCRIPT
+
 echo -e "Making first backup..."
-tar -czf $BACKUPFULLPATH/$FILENAME $MCDIR/world*
+$BACKUPFULLPATH/$BACKUPSCRIPT
